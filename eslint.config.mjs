b77057d791b496f9593c76f9cd2be2094a33efff
joel Figueroa <joelmartinez@epsilontech.ai>
2025-09-17 11:51:41 -0600

    feat: Update ESLint configuration to include Prettier and Airbnb style guides, and add custom rules for max line length and quote style.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:prettier/recommended", "airbnb", "airbnb/hooks", "airbnb-typescript"),
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +19,15 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "max-len": [
+        "error",
+        {
+          code: 140
+        },
+      ],
+      quotes: [2, "single", { avoidEscape: true }],
+    },
   },
 ];
 
